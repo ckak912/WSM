a1e07569744e027806582ea641b0be37f1dc56e4
Charlie Kake-White <charliekakewhite@gmail.com>
2023-09-05 13:52:46 +1200

    first contact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import Clients from '../components/Clients';
 import Cta from '../components/Cta';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
-import Intro from '../components/Intro';
 import Portfolio from '../components/Portfolio';
 import Services from '../components/Services';
 
@@ -12,11 +11,7 @@ const Home = () => {
     return (
         <>
             <Hero />
-            <Intro />
             <Services />
-            <Portfolio />
-            <Clients />
-            <Cta/>
             <Footer />
         </>
 
