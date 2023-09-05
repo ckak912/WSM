cf44c0e4aa655a03410d0e217386a5cfcf66e108
ckak912 <ckak912@aucklanduni.ac.nz>
2023-09-05 14:30:05 +1200

    removed unused vars

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Clients from '../components/Clients';
-import Cta from '../components/Cta';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
-import Portfolio from '../components/Portfolio';
 import Services from '../components/Services';
 
 
