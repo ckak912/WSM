b9f5766c3d74d21d5b92b8850cea06b26cb21bcc
charliekw411 <142861574+charliekw411@users.noreply.github.com>
2024-01-23 05:51:50 +0000

    fix: changed navbar heights

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -19,11 +19,13 @@ const NavBar = () => {
     }, [top]);
 
     return (
-        <nav className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out mb-16 ${!top && 'bg-white shadow-lg'}`}>
+        <nav className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out mb-16 ${!top && 'bg-white shadow-lg'}`} style={{ height: '25%' }}>
             <div className="flex flex-row justify-between items-center py-2">
                 <div className="flex flex-row justify-center md:px-12 md:mx-12 items-center text-center font-semibold">
                     <Link to="/">
-                        <img src={logoImage} alt="Logo" className="w-auto" />
+                        <div style={{ maxHeight: '100%' }}>
+                            <img src={logoImage} alt="Logo" className="w-auto max-h-full" />
+                        </div>
                     </Link>
                 </div>
                 <div className="group flex flex-col items-center">
