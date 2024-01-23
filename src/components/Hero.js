f80292cf534b6e03ae873be15d095807a027cfa9
ckak912 <ckak912@aucklanduni.ac.nz>
2024-01-23 15:52:45 +1300

    changed contact component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,12 +13,12 @@ const Hero = () => {
                 <div className="m-auto overflow-hidden mx-4 mt-8 lg:mt-4 p-2 md:p-12 h-4/6" data-aos="zoom-in">
                     <div className="flex flex-col lg:flex-row py-8 justify-between text-center lg:text-left">
                         <div className="lg:w-1/2 flex flex-col justify-center" data-aos="zoom-in" data-aos-delay="200">
-                            <h1 className="mb-5 md:text-5xl text-3xl font-bold text-red-600">
+                            <h1 className="mb-5 md:text-5xl text-3xl font-bold text-black-600">
                                 Elevating your Service Experience
                             </h1>
                             <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">We pride ourselves on being a close-knit, proficient, and seasoned team, dedicated to delivering outstanding automotive solutions. Our primary goal is to provide top-tier services that set the bar for quality and affordability.</div>
                             <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
-                            <Link to="/contact" className="text-white bg-red-600 hover:bg-red-500 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
+                            <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
                                 Get in touch
                                 <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
